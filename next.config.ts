8682f6509f0e6ddabedba27a0da28c97a3e064db
tommyzki <your_personal_email@example.com>
2025-06-05 10:57:47 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/tommyzki-cv',
-  assetPrefix: '/tommyzki-cv/',
   typescript: {
     ignoreBuildErrors: true,
   },
