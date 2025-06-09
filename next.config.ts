2324fc5400efd8218be06875358bb30f16dfb42c
Tommy M <miyazaki.tommy@gmail.com>
2025-06-09 08:34:56 +0000

    update next config

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'miro.medium.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
